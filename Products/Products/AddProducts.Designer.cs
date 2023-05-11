6d14a470a8ff4fe011c076c0ec136d189a567d27
geki7 <83777643+geki7@users.noreply.github.com>
2023-05-11 08:38:53 +0200

    Add files via upload

---
+++ Products/Products/AddProducts.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Products
+{
+    partial class frmAddProducts
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtbName = new System.Windows.Forms.TextBox();
+            this.txtbQuantity = new System.Windows.Forms.TextBox();
+            this.txtbPrice = new System.Windows.Forms.TextBox();
+            this.cmbCategory = new System.Windows.Forms.ComboBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtbName
+            // 
+            this.txtbName.Location = new System.Drawing.Point(24, 47);
+            this.txtbName.Name = "txtbName";
+            this.txtbName.Size = new System.Drawing.Size(191, 20);
+            this.txtbName.TabIndex = 0;
+            // 
+            // txtbQuantity
+            // 
+            this.txtbQuantity.Location = new System.Drawing.Point(24, 109);
+            this.txtbQuantity.Name = "txtbQuantity";
+            this.txtbQuantity.Size = new System.Drawing.Size(191, 20);
+            this.txtbQuantity.TabIndex = 1;
+            // 
+            // txtbPrice
+            // 
+            this.txtbPrice.Location = new System.Drawing.Point(24, 170);
+            this.txtbPrice.Name = "txtbPrice";
+            this.txtbPrice.Size = new System.Drawing.Size(191, 20);
+            this.txtbPrice.TabIndex = 2;
+            // 
+            // cmbCategory
+            // 
+            this.cmbCategory.FormattingEnabled = true;
+            this.cmbCategory.Location = new System.Drawing.Point(24, 228);
+            this.cmbCategory.Name = "cmbCategory";
+            this.cmbCategory.Size = new System.Drawing.Size(191, 21);
+            this.cmbCategory.TabIndex = 3;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(24, 283);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 4;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(140, 283);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 5;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // frmAddProducts
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(269, 352);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.cmbCategory);
+            this.Controls.Add(this.txtbPrice);
+            this.Controls.Add(this.txtbQuantity);
+            this.Controls.Add(this.txtbName);
+            this.Name = "frmAddProducts";
+            this.Text = "Add products";
+            this.Load += new System.EventHandler(this.frmAddProducts_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtbName;
+        private System.Windows.Forms.TextBox txtbQuantity;
+        private System.Windows.Forms.TextBox txtbPrice;
+        private System.Windows.Forms.ComboBox cmbCategory;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
---
+++ Products/Products/AddProducts.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Products
+{
+    public partial class frmAddProducts : Form
+    {
+        public frmAddProducts()
+        {
+            InitializeComponent();
+        }
+
+        private void frmAddProducts_Load(object sender, EventArgs e)
+        {
+            fillCategory();
+        }
+
+        private void fillCategory()
+        {
+            List<Category> kategorija;
+            using(var context = new EF_DBEntities())
+            {
+                kategorija = context.Categories.ToList();
+            }
+            cmbCategory.DataSource = kategorija;
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            using(var context = new EF_DBEntities())
+            {
+                string name = txtbName.Text;
+                int quntity = int.Parse(txtbQuantity.Text);
+                int unitPrice = int.Parse(txtbPrice.Text);
+                Category kategorija = cmbCategory.SelectedItem as Category;
+                context.Categories.Attach(kategorija);
+
+                Product dodajNovi = new Product
+                {
+                    Name = name,
+                    Quantity = quntity,
+                    UnitPrice = unitPrice,
+                    Category = kategorija
+                };
+
+                context.Products.Add(dodajNovi);
+                context.SaveChanges();
+            }
+            Close();
+        }
+    }
+}
