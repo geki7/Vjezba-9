b1e4a45ae8595207127faba419dda0096b8220bc
geki7 <83777643+geki7@users.noreply.github.com>
2023-05-11 08:45:53 +0200

    Add files via upload

---
+++ Products/Products/Products.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Products
+{
+    partial class frmProducts
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvProducts = new System.Windows.Forms.DataGridView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvProducts
+            // 
+            this.dgvProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProducts.Location = new System.Drawing.Point(12, 24);
+            this.dgvProducts.Name = "dgvProducts";
+            this.dgvProducts.Size = new System.Drawing.Size(766, 295);
+            this.dgvProducts.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(483, 325);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(144, 75);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add new";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(633, 325);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(144, 75);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // frmProducts
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 425);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dgvProducts);
+            this.Name = "frmProducts";
+            this.Text = "Products";
+            this.Load += new System.EventHandler(this.frmProducts_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvProducts;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ Products/Products/Products.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Products
+{
+    public partial class frmProducts : Form
+    {
+        public frmProducts()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            frmAddProducts form  = new frmAddProducts();    
+            form.ShowDialog();
+            Osvjezi();
+        }
+
+        private Object showProdcuts()
+        {
+            using(var context = new EF_DBEntities())
+            {
+                return context.Products.ToList();
+            }
+        }
+
+        private void Osvjezi()
+        {
+            dgvProducts.DataSource = showProdcuts();
+            dgvProducts.Columns["Category"].Visible = false;
+        }
+
+        private void frmProducts_Load(object sender, EventArgs e)
+        {
+            Osvjezi();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            Product brisanjeProduct = dgvProducts.CurrentRow.DataBoundItem as Product;
+            using(var context = new EF_DBEntities())
+            {
+                context.Products.Attach(brisanjeProduct);
+                context.Products.Remove(brisanjeProduct);
+                context.SaveChanges();
+            }
+            Osvjezi();
+        }
+    }
+}
