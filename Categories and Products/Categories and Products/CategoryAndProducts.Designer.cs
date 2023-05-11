60b5c8b3b3c7174e1ba7a679bdf79faaf9c508ed
geki7 <83777643+geki7@users.noreply.github.com>
2023-05-11 08:35:44 +0200

    Add files via upload

---
+++ Categories and Products/Categories and Products/CategoryAndProducts.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Categories_and_Products
+{
+    partial class CategoryandProducts
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvCatergory = new System.Windows.Forms.DataGridView();
+            this.dgvProducts = new System.Windows.Forms.DataGridView();
+            this.txtbProductName = new System.Windows.Forms.TextBox();
+            this.txtbQuantity = new System.Windows.Forms.TextBox();
+            this.txtbUnitPrice = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCatergory)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvCatergory
+            // 
+            this.dgvCatergory.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCatergory.Location = new System.Drawing.Point(12, 28);
+            this.dgvCatergory.Name = "dgvCatergory";
+            this.dgvCatergory.Size = new System.Drawing.Size(266, 279);
+            this.dgvCatergory.TabIndex = 0;
+            this.dgvCatergory.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvCatergory_CellContentClick);
+            // 
+            // dgvProducts
+            // 
+            this.dgvProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProducts.Location = new System.Drawing.Point(333, 28);
+            this.dgvProducts.Name = "dgvProducts";
+            this.dgvProducts.Size = new System.Drawing.Size(663, 279);
+            this.dgvProducts.TabIndex = 1;
+            this.dgvProducts.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvProducts_CellContentClick);
+            // 
+            // txtbProductName
+            // 
+            this.txtbProductName.Location = new System.Drawing.Point(333, 348);
+            this.txtbProductName.Name = "txtbProductName";
+            this.txtbProductName.Size = new System.Drawing.Size(169, 20);
+            this.txtbProductName.TabIndex = 2;
+            // 
+            // txtbQuantity
+            // 
+            this.txtbQuantity.Location = new System.Drawing.Point(508, 348);
+            this.txtbQuantity.Name = "txtbQuantity";
+            this.txtbQuantity.Size = new System.Drawing.Size(98, 20);
+            this.txtbQuantity.TabIndex = 3;
+            // 
+            // txtbUnitPrice
+            // 
+            this.txtbUnitPrice.Location = new System.Drawing.Point(612, 348);
+            this.txtbUnitPrice.Name = "txtbUnitPrice";
+            this.txtbUnitPrice.Size = new System.Drawing.Size(98, 20);
+            this.txtbUnitPrice.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(330, 332);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Product name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(505, 332);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Quantity:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(609, 332);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Unit price:";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(824, 346);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 8;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(921, 345);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 9;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // CategoryandProducts
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1012, 450);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtbUnitPrice);
+            this.Controls.Add(this.txtbQuantity);
+            this.Controls.Add(this.txtbProductName);
+            this.Controls.Add(this.dgvProducts);
+            this.Controls.Add(this.dgvCatergory);
+            this.Name = "CategoryandProducts";
+            this.Text = "Category and Products";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCatergory)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvCatergory;
+        private System.Windows.Forms.DataGridView dgvProducts;
+        private System.Windows.Forms.TextBox txtbProductName;
+        private System.Windows.Forms.TextBox txtbQuantity;
+        private System.Windows.Forms.TextBox txtbUnitPrice;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ Categories and Products/Categories and Products/CategoryAndProducts.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Categories_and_Products
+{
+    public partial class CategoryandProducts : Form
+    {
+        public CategoryandProducts()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dgvCatergory.DataSource = GetCategory();
+            dgvCatergory.Columns["Products"].Visible = false;
+        }
+
+        private object GetCategory()
+        {
+            using(var context = new EF_DBEntities())
+            {
+                return context.Categories.ToList();
+            }
+        }
+
+        private void showProducts(Category kategorija)
+        {
+            List<Product> proizvodi;
+            using(var context = new EF_DBEntities())
+            {
+                context.Categories.Attach(kategorija);
+                proizvodi = kategorija.Products.ToList();
+            }
+            dgvProducts.DataSource = proizvodi;
+        }
+
+        private void dgvCatergory_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            Category odabranaKat = dgvCatergory.CurrentRow.DataBoundItem as Category;
+            if (odabranaKat != null)
+            {
+                showProducts(odabranaKat);
+            }
+        }
+
+        private void dgvProducts_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+           
+        }
+        private Category kat;
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            using (var context = new EF_DBEntities())
+            {
+                string name = txtbProductName.Text;
+                int quantity = int.Parse(txtbQuantity.Text);
+                int unitPrice = int.Parse(txtbUnitPrice.Text);
+                kat = dgvCatergory.CurrentRow.DataBoundItem as Category;
+                context.Categories.Attach(kat);
+
+                Product newProdcut = new Product
+                {
+                    Name = name,
+                    Quantity = quantity,
+                    UnitPrice = unitPrice,
+                    Category = kat,
+                };
+
+                context.Products.Add(newProdcut);
+                context.SaveChanges();
+            }
+        }
+    }
+}
